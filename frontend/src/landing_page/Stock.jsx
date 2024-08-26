5130eff076b547db662478550026d23dd22efd2e
Prasun2004 <166635147+Prasun2004@users.noreply.github.com>
2024-08-26 21:27:00 +0530

    Add files via upload

---
+++ frontend/src/landing_page/Stock.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Stock() {
+    return ( 
+        <div> 
+          <p>hi</p>
+          <a href='/all'>click</a>
+        </div>
+     );
+}
+
+export default Stock;
